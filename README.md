9a40c7df12244cc63fbdafa5164933e9d62ed3e5
bielbarros <gabrielbarrosg11@gmail.com>
2024-06-11 21:33:57 -0300

    add Estruturas condicionais

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # SantanderCoders2024
+Repositorio criado para armazenar e testar os conhecimentos obtidos no programa Santander Coders 2024, prentendo testar e documentar meus aprendizados do básico ao avançado na linguagem JAVA.
+
+Será usado frameworks, banco de dados, API's e IA.
+
+
+### Me conheça!
+
+[![E-mail](https://img.shields.io/badge/-Email-000?style=for-the-badge&logo=microsoft-outlook&logoColor=E94D5F)](mailto:gabrielbarrosg11@gmail.com)
+[![LinkedIn](https://img.shields.io/badge/-LinkedIn-000?style=for-the-badge&logo=linkedin&logoColor=30A3DC)](https://www.linkedin.com/in/gabriel-sbarros/)
+[![Meu portifólio](https://img.shields.io/badge/Github-000?style=for-the-badge&logo=Github&logoColor=3)](https://github.com/bielbarros)
+
+
+
+### Tecnologias usadas:
+
+
+![JAVA](https://img.shields.io/badge/Java-00?style=for-the-badge&logo=openjdk&logoColor=467)
+![vscode](https://img.shields.io/badge/Vscode-00?style=for-the-badge&logo=visualstudiocode&logoColor=white)
+![Git](https://img.shields.io/badge/Git-00?style=for-the-badge&logo=git&logoColor=E94D5F)
+[![GitHub](https://img.shields.io/badge/GitHub-00?style=for-the-badge&logo=github&logoColor=3A3DC)](https://github.com/bielbarros)
--- src/Aprendizado.java
+++ src/Aprendizado.java
@@ -1,2 +1,118 @@
-package PACKAGE_NAME;public class Aprendizado {
+public class Aprendizado {
+
+    public static void main(String[] args) {
+
+            /*
+            TIPOS DE VARIÁVEIS;
+
+            String meuNome = Gabriel;
+            byte idade = 123;
+            short ano = 2024;
+            int cep = 232532535; // se começar com 0 troque para string por conta da compilação.
+            long cpf = 83535345376L; // se começar com '0' troque para string por conta da compilação.
+            float pi = 3.14F;
+            double salario = 1275.33;
+            boolean = Valor lógico true/false, imutável após definido;
+            char: simbolo, letra, contanto que seja 1 único caractere.
+
+
+            // Para variavel imutável usar o ("final" tipoDaVariavel NOME_DA_VARIAVEL = valorAtribuido)
+
+            OPERADORES LÓGICOS;
+
+            int soma = a + b;
+            int subtracao = a - b;
+            int multiplicacao = a * b;
+            int divisao = a / b;
+
+
+
+            TABELA VERDADE
+
+            Operador && (AND)
+
+            true && true = true
+            true && false = false
+            false && true = false
+            false && false = false
+
+            boolean fimDeSemana = true;
+            boolean fazendoSol = true;
+            boolean vamosAPraia = fimDeSemana && fazendoSol;
+
+
+            Operador || (OR)
+
+            true || true = true
+            true || false = true
+            false || true = true
+            false || false = false
+
+               TESTE DE OPERADOR TRICENTENARIO*/
+
+        boolean meuTeste = true;
+
+        String mensagem = meuTeste ? "Funcionou!" : "Não funcionou!";
+
+        System.out.println(mensagem);
+
+
+        /* ESTRUTUTURAS CONDICIONAIS
+
+        If-else
+
+        int nota = 90
+
+            if (nota >= 70) {
+                System.out.println("Aluno Aprovado!")
+            }    else {
+                System.out.println("Não aprovado!")
+               }
+
+            if (nota >= 80) {
+                System.out.println("Graduação A");
+            } else if (nota < 80 && > 70) {
+                System.out.println("Graduação B");
+            } else if (nota < 70 && > 60) {
+                System.out.println("Graduação C");
+            } else if (nota < 60 && > 0) {
+                System.out.println("Graduação D");
+            } else {
+
+            }
+System.out.println("Nota inválida");
+
+            CÓDIGO DE FORMA MAIS SOFISTICADA:
+
+            String = graduacao;
+
+            if (graduacao >= 80) {
+                graduacao = "A";
+            } else if (nota < 80 && > 70) {
+                graduacao = "B";
+            } else if (nota < 70 && > 60) {
+                graduacao = "C";
+            } else if (nota < 60 && > 0) {
+                graduacao = "D";
+            } else {
+                graduacao = "";
+            }
+
+            switch (graduação);
+                case "A":
+                case "B":
+                    System.out.println("Aluno aprovado!");
+                    break;
+                case "C":
+                case "D":
+                    System.out.println("Não aprovado");
+                    break;
+                default:
+                    System.out.println("Graduação inválida!");
+
+
+         */
+
+
+    }
 }
